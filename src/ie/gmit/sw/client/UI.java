fe89d243498542312eda8b1df5b4e9208d32d335
smurfgalway <g00323639@gmit.ie>
2017-01-07 03:11:29 +0000

    code

--- src/ie/gmit/sw/client/UI.java
+++ src/ie/gmit/sw/client/UI.java
@@ -1,5 +1,25 @@
 package ie.gmit.sw.client;
+import java.util.*;
+public class UI {
 
-public interface UI {
+public static void main(String[] args) {	
+	
+	Scanner userInput = new Scanner(System.in);
+	
+	int i =0;
+	
+	System.out.println("1. Connect to Server");
+	System.out.println("2. Print File Listing");
+	System.out.println("3. Download File");
+	System.out.println("4. Quit");
+	
+	System.out.println("\n Type Option []>");
+	while (i < 1) {
+		
+		if (""){
+			
+		}
 
+	}
+}
 }
